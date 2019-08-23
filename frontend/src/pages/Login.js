7c029c5710faebbca9de2cbc55939000be9d3500
Bruno Bach <42494225+brunobach@users.noreply.github.com>
2019-08-23 20:10:09 -0300

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -12,7 +12,7 @@ export default function Login({history}){
             e.preventDefault()
 
             const response = await api.post('/devs', {
-                username,
+                username.toLowerCase(),
             })
 
             const {_id} = response.data
